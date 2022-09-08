1147533070fa797a6b30d5d934cb138590e60fa4
xunruan <xunruan@icloud.com>
2022-09-08 17:55:35 -0400

    Set up static files

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/"  # It is used by static tag in template
+STATICFILES_DIRS = [BASE_DIR / "static"]    # It is used to include additional folder for collectstatic
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
