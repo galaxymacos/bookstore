5d653b81fd8233c1765b981605b792ddb4d6fcd3
xunruan <xunruan@icloud.com>
2022-09-08 17:36:44 -0400

    Add test to signup page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from accounts.forms import CustomUserCreationForm
+from accounts.views import SignupPageView
 
 
 # Create your tests here.
@@ -22,9 +26,30 @@ class CustomUserTests(TestCase):
         admin_user = User.objects.create_superuser(
             username='admin',
             password='testpass123',
-            email="admin@email.com",)
+            email="admin@email.com", )
         self.assertEqual(admin_user.username, 'admin')
         self.assertEqual(admin_user.email, 'admin@email.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTest(TestCase):
+    def setUp(self) -> None:
+        url = reverse("accounts:signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, "Sign Up")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")    # get form from url response context
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
