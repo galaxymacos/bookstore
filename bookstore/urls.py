e4a1ad06f0a550f8fb339be2da8bb82e11734531
xunruan <xunruan@icloud.com>
2022-09-08 16:53:19 -0400

    Add custom signup url, view, and template

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),     # Use Djagno log in/out, and sign up
     path("", include("pages.urls")),
+    path("accounts/", include("accounts.urls"))
 ]
