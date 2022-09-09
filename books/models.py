9bf925e154f55a2a0c735e7bb8a06a12f2094d03
xunruan <xunruan@icloud.com>
2022-09-09 18:54:01 -0400

    Recreate book model using uuid as id
    UUIDField
    <uuid:pk>
    exec web rm -r books/migrations
    docker-compose.yml
    docker volume ls
    docker volume rm books_postgres_data

--- books/models.py
+++ books/models.py
@@ -1,9 +1,12 @@
+import uuid
+
 from django.db import models
 from django.urls import reverse
 
 
 # Create your models here.
 class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
