10d3850ad50b9feee5601b44f5463be39b8a2832
xunruan <xunruan@icloud.com>
2022-09-10 11:41:15 -0400

    Support User Image Upload
    static media url
    
    pillow
    
    ImageField
    
    MEDIA_URL / MEDIA_ROOT

--- books/models.py
+++ books/models.py
@@ -11,6 +11,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
     def __str__(self):
         return self.title
