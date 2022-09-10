40c1f4b178ee7c8a93340beebb3b275eb150d324
xunruan <xunruan@icloud.com>
2022-09-10 01:44:57 -0400

    Test a one-to-many relation

--- books/tests.py
+++ books/tests.py
@@ -1,17 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 
 # Create your tests here.
 class BookTests(TestCase):
 
+    user = None
+    book = None
+
     @classmethod
     def setUpTestData(cls):
         # The class-level atomic block described above allows the creation of initial data at the class level,
         # once for the whole TestCase.
+        cls.user = get_user_model().objects.create_user(username="reviewuser", email="reviewuser@email.com", password="testpass123")
         cls.book = Book.objects.create(title='Harry Potter', author='J.K. Rowling', price='25.00')
+        cls.review = Review.objects.create(book=cls.book, author=cls.user, review="An excellent review")
 
     def test_book_listing(self):
         self.assertEqual(f'{self.book.title}', 'Harry Potter')
@@ -30,4 +36,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, "books/book_detail.html")
